bbe8eecaba6f2c8ce6b925f4ca8a60ca493857ab
Jorge Casta <jorgerochin0102@gmail.com>
2023-06-23 15:51:33 -0700

    Created sign up componenet, the form input component, and button component

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -1,14 +1,13 @@
-// Import necessary functions and objects from Firebase and Firestore libraries
 import { initializeApp } from 'firebase/app';
 import {
   getAuth,
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword,
 } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
-// Firebase configuration object containing project-specific settings
 const firebaseConfig = {
   apiKey: "AIzaSyDOYpbB2vvZcpwyn7OVL2uf4BRJjq3hrPg",
   authDomain: "crwn-clothing-db-d4eb9.firebaseapp.com",
@@ -18,53 +17,53 @@ const firebaseConfig = {
   appId: "1:28854744568:web:ec7d938741ab09ece4920d"
 };
 
-// Initialize the Firebase app with the provided configuration
 const firebaseApp = initializeApp(firebaseConfig);
 
-// Create an instance of the GoogleAuthProvider class for authentication
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-// Set custom parameters on the provider to prompt user account selection
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
   prompt: 'select_account',
 });
 
-// Initialize the authentication object
 export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
-// Function to initiate sign-in process using Google account
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
-
-// Initialize the Firestore database object
 export const db = getFirestore();
 
-// Function to create a user document in the Firestore database
-export const createUserDocumentFromAuth = async (userAuth) => {
-  // Create a reference to the user document using the user ID
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+
   const userDocRef = doc(db, 'users', userAuth.uid);
 
-  // Retrieve the user document snapshot
   const userSnapshot = await getDoc(userDocRef);
 
-  // Check if the user document doesn't exist
   if (!userSnapshot.exists()) {
-    // Extract user details from userAuth object
     const { displayName, email } = userAuth;
     const createdAt = new Date();
 
     try {
-      // Create the user document in the Firestore database
       await setDoc(userDocRef, {
         displayName,
         email,
         createdAt,
+        ...additionalInformation,
       });
     } catch (error) {
-      // Log an error message if there's an error creating the user document
-      console.log('Error creating the user', error.message);
+      console.log('error creating the user', error.message);
     }
   }
 
-  // Return the user document reference
   return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
 };
